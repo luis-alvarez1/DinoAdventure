8612427a6f6c8fa19541f2a2de7612edae907dec
Luis Alvarez <lealvarezp@unac.edu.co>
2020-05-20 13:32:00 -0500

    correccion de posicion del principal

--- src/GamePlay.js
+++ src/GamePlay.js
@@ -4,13 +4,7 @@ var GamePlay = {
   preload: function () {
     game.load.image("background", "assets/img/background.png");
     game.load.image("ground", "assets/img/ground.jpeg");
-    game.load.spritesheet(
-      "dinosaur",
-      "assets/img/principal.png",
-      56,
-      47,
-      13
-    );
+    game.load.spritesheet("dinosaur", "assets/img/principal.png", 56, 47, 13);
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
@@ -19,7 +13,8 @@ var GamePlay = {
   create: function () {
     this.background = game.add.tileSprite(0, 0, 1000, 413, "background");
     this.ground = game.add.tileSprite(0, 350, 1000, 100, "ground");
-    this.dino = game.add.sprite(game.with / 2, game.height / 2, "dinosaur");
+    this.dino = game.add.sprite(100, 340, "dinosaur");
+    this.dino.anchor.setTo(0.5);
     this.dino.frame = 13;
   },
   update: function () {},
