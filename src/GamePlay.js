9ff79b09261ec85685ec3b5ca833c98e74be274e
Luis Alvarez <lealvarezp@unac.edu.co>
2020-05-20 17:17:32 -0500

    implementacion de animaciones de movimiento y espera

--- src/GamePlay.js
+++ src/GamePlay.js
@@ -2,6 +2,7 @@ var game = new Phaser.Game(1000, 418, Phaser.CANVAS, "game_block");
 
 var GamePlay = {
   preload: function () {
+    game.stage.backgroundColor = "#000000";
     game.load.image("background", "assets/img/background.png");
     game.load.image("ground", "assets/img/ground.jpeg");
     game.load.spritesheet("dinosaur", "assets/img/principal.png", 56, 47, 13);
@@ -15,9 +16,55 @@ var GamePlay = {
     this.ground = game.add.tileSprite(0, 350, 1000, 100, "ground");
     this.dino = game.add.sprite(100, 340, "dinosaur");
     this.dino.anchor.setTo(0.5);
-    this.dino.frame = 13;
+    this.dino.frame = 12;
+
+    this.dino.animations.add(
+      "walk",
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      10,
+      true
+    );
+
+    this.movement = game.input.keyboard.createCursorKeys();
+    this.jumpKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    game.physics.arcade.enable(this.dino);
+
+    this.dino.body.colliderWorldBounds = true;
   },
-  update: function () {},
+  update: function () {
+    if (this.direction == "wating") {
+      this.dino.frame = 12;
+    }
+    if (this.movement.right.isDown) {
+      this.dino.scale.setTo(1);
+
+      this.dino.position.x += 1;
+      this.dino.animations.play("walk");
+
+      if (this.direction != "right") {
+        this.direction = "right";
+      }
+    } else if (this.movement.left.isDown) {
+      this.dino.scale.setTo(-1, 1);
+
+      this.dino.position.x -= 1;
+      this.dino.animations.play("walk");
+
+      if (this.direction != "left") {
+        this.direction = "left";
+      }
+    } else if (this.jumpKey.isDown) {
+      console.log("space");
+    } else {
+      if (this.direction != "waiting") {
+        this.dino.animations.stop();
+      }
+      this.direction = "wating";
+    }
+  },
 };
 
 game.state.add("Main", GamePlay);
