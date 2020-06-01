61ba92d47e78b80c2119e6f8f826b57105f7cb06
Luis Alvarez <lealvarezp@unac.edu.co>
2020-06-01 18:35:03 -0500

    Auto stash before merge of "dev" and "origin/dev"

--- src/GamePlay.js
+++ src/GamePlay.js
@@ -8,37 +8,55 @@ var GamePlay = {
     game.load.spritesheet("dinosaur", "assets/img/principal.png", 56, 47, 13);
     game.load.spritesheet("enemy1", "assets/img/enemigo1.png", 59, 46, 10);
     game.load.spritesheet("lives", "assets/img/vidas.png", 107, 40, 4);
-    game.load.spritesheet("titulodino", "assets/img/titulodino.png", 257, 109, 1);
-    game.load.spritesheet("tituloadventure", "assets/img/tituloadventure.png", 317, 76, 1);
+    game.load.spritesheet(
+      "titulodino",
+      "assets/img/titulodino.png",
+      257,
+      109,
+      1
+    );
+    game.load.spritesheet(
+      "tituloadventure",
+      "assets/img/tituloadventure.png",
+      317,
+      76,
+      1
+    );
     game.load.spritesheet("gameOver", "assets/img/gameOver.png", 161, 112, 1);
     game.load.spritesheet("buttonPlay", "assets/img/playGame.png", 156, 43, 1);
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
   },
-  
+
   create: function () {
-    
-    
     this.background = game.add.tileSprite(0, 0, 1000, 413, "background");
     this.ground = game.add.tileSprite(0, 350, 1000, 100, "ground");
-    
+
     this.live = game.add.sprite(10, 10, "lives");
     this.live.frame = 0;
-    
+
     this.titleoDino = game.add.sprite(390, 50, "titulodino");
     this.titleoDino.frame = 0;
-    
+
     this.titleoAdventure = game.add.sprite(350, 150, "tituloadventure");
     this.titleoAdventure.frame = 0;
-    
+
     this.gameOver = game.add.sprite(440, 100, "gameOver");
     this.gameOver.frame = 0;
     this.gameOver.visible = false;
 
+    this.buttonPlay = game.add.button(
+      430,
+      220,
+      "buttonPlay",
+      actionOnClick,
+      this,
+      2,
+      1,
+      0
+    );
 
-    this.buttonPlay = game.add.button(430, 220, "buttonPlay",actionOnClick, this,2,1,0 );
-    
     this.dino = game.add.sprite(100, 340, "dinosaur");
     this.dino.anchor.setTo(0.5);
     this.dino.frame = 12;
@@ -71,24 +89,22 @@ var GamePlay = {
     this.enemy1.animations.play("walking");
     var tween = game.add.tween(this.enemy1);
     tween.to({ x: 600 }, 5000, Phaser.Easing.Linear.None, true, 0, 1000, true);
-    
 
     this.currentScore = 0;
     var style = {
-      font: 'bold 20pt Arial',
-      fill: 'white',
-      align: 'center'
-    }
-    this.scoreText = game.add.text(game.width/2, 40, '0', style);
+      font: "bold 20pt Arial",
+      fill: "white",
+      align: "center",
+    };
+    this.scoreText = game.add.text(game.width / 2, 40, "0", style);
     this.play = false;
-    function actionOnClick () {
+    function actionOnClick() {
       this.play = true;
-      
     }
   },
-  
-  winScore: function (){
-    this.currentScore+=100;
+
+  winScore: function () {
+    this.currentScore += 100;
     this.scoreText.text = this.currentScore;
   },
   render: function () {
@@ -103,7 +119,7 @@ var GamePlay = {
 
     return new Phaser.Rectangle(x0, y0, width, height);
   },
-  
+
   isRectangleOverlapping: function (rect1, rect2) {
     //se valida si los recangulos se tocan o se sobreponen y devuelve false si no es así
     if (rect1.x > rect2.x + rect2.width || rect2.x > rect1.x + rect1.width) {
@@ -116,31 +132,30 @@ var GamePlay = {
   },
   update: function () {
     var play = this.play;
-    
-    if (play==true) {
+
+    if (play == true) {
       this.titleoDino.visible = false;
       this.titleoAdventure.visible = false;
       this.buttonPlay.visible = false;
-      
 
       if (this.direction == "wating") {
         this.dino.frame = 12;
       }
       if (this.movement.right.isDown) {
         this.dino.scale.setTo(1);
-  
+
         this.dino.position.x += 2;
         this.dino.animations.play("walk");
-  
+
         if (this.direction != "right") {
           this.direction = "right";
         }
       } else if (this.movement.left.isDown) {
         this.dino.scale.setTo(-1, 1);
-  
+
         this.dino.position.x -= 2;
         this.dino.animations.play("walk");
-  
+
         if (this.direction != "left") {
           this.direction = "left";
         }
@@ -151,10 +166,10 @@ var GamePlay = {
         if (this.direction != "waiting") {
           this.dino.animations.stop("walk");
         }
-  
+
         this.direction = "wating";
       }
-      
+
       if (
         this.isRectangleOverlapping(
           this.getBounds(this.dino),
@@ -165,11 +180,10 @@ var GamePlay = {
         console.log("enemigo muerto");
         this.enemy1.animations.stop();
         this.enemy1.frame = 9;
-        if (this.enemy1.frame = 9 && this.enemy1.visible){
-          this.enemy1.visible = false;  
+        if (this.enemy1.frame == 9 && this.enemy1.visible) {
+          this.enemy1.visible = false;
           this.winScore();
         }
-        
       } else if (
         this.isRectangleOverlapping(
           this.getBounds(this.dino),
@@ -177,20 +191,19 @@ var GamePlay = {
         ) &&
         this.direction != "bite"
       ) {
-          this.live.frame = 3;
-         if(this.live.frame = 3  ){
-            this.gameOver.visible = true;
-            this.dino.animations.stop();
-            console.log("muerto");
-         }
-        
+        this.live.frame = 3;
+        if (this.live.frame == 3) {
+          this.gameOver.visible = true;
+          this.dino.animations.stop();
+          console.log("muerto");
+        }
       }
-  
+
       //movimiento enemigo 1
       var limitRight = 800;
       var limitLeft = 600;
       var posicionxEnemy1 = this.enemy1.x;
-  
+
       if (posicionxEnemy1 == limitRight) {
         this.enemy1.scale.setTo(1, 1);
       }
@@ -198,7 +211,6 @@ var GamePlay = {
         this.enemy1.scale.setTo(-1, 1);
       }
     }
-    
   },
 };
 
